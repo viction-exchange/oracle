5abb56964d47683991e3bf35426001831c2c538b
Bakaoh <batatsar@gmail.com>
2024-06-17 14:55:51 +0700

    Add WBTC and WETH testnet

--- src/UnsafePriceKeeper.js
+++ src/UnsafePriceKeeper.js
@@ -15,6 +15,8 @@ const priceIds = [
   '0xf80ba6864e3f1b36c873bcb2767079d5fb86cf04855e714b2a0f30d7e0830a24', // VIC
   '0x2dd14c7c38aa7066c7a508aac299ebcde5165b07d5d9f2d94dfbfe41f0bc5f2e', // C98
   '0x8f218655050a1476b780185e89f19d2b1e1f49e9bd629efad6ac547a946bf6ab', // CUSD
+  '0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43', // WBTC
+  '0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace', // WETH
 ]
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms))
